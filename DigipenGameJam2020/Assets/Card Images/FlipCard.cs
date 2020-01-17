6f7fdf01a5f11239b325a0456f8199b4a9495eb3
Il <rtd990@naver.com>
2020-01-17 20:55:30 +0900

    First Minigame will be implemented until tomorrow

---
+++ DigipenGameJam2020/Assets/Card Images/FlipCard.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipCard : MonoBehaviour
+{
+    public int fps = 60;
+    public int rotatePerDegreeSecond = 180;
+    public bool isFaceUp = false;
+
+    const float FLIP_LIMIT_DEGREE = 180.0f;
+
+    float waitTime;
+    bool isAnimationProcessing = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waitTime = 1.0f / fps;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        if(isAnimationProcessing)
+        {
+            return;
+        }
+        StartCoroutine(flip());
+
+    }
+
+    IEnumerator flip()
+    {
+        isAnimationProcessing = true;
+
+        bool done = false;
+        while(!done)
+        {
+            float degree = rotatePerDegreeSecond * Time.deltaTime;
+            if (isFaceUp)
+            {
+                degree = -degree;
+            }
+
+            transform.Rotate(new Vector3(0, degree, 0));
+            if(FLIP_LIMIT_DEGREE < transform.eulerAngles.y)
+            {
+                transform.Rotate(new Vector3(0, -degree, 0));
+                done = true;
+            }
+
+            yield return new WaitForSeconds(waitTime);
+        }
+
+        isFaceUp = !isFaceUp;
+        isAnimationProcessing = false;
+    }
+}
