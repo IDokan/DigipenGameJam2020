71783078b82a2f6e7de23c788b0b778587c895f9
Il <rtd990@naver.com>
2020-01-17 15:59:32 +0900

    test_Push

---
+++ Assets/Status.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Status : MonoBehaviour
+{
+    // it is going to be up & down, up & down
+    public int intelligence { get; set; } = 100;
+
+    // the value is going to be decrease
+    public int health { get; set; } = 100;
+
+
+    // the values are going to be increse
+    public int fatigue { get; set; } = 100;
+    public int urin { get; set; } = 0;
+    public int hunger { get; set; } = 0;
+    public int stress { get; set; } = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
